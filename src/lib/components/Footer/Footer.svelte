1e7cc4352d62d7896b7203dcf8d6cc3a5f0e8a32
Schaeferjo <mail@joschaefer.com>
2021-11-20 17:02:24 +0100

    feat: text corrections

--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -23,7 +23,7 @@
         51069 Köln<br />
         <br />
         Telefon: +49 (0)221 - 68 36 91<br />
-        Fax: +40 (0)221 - 68 97 797<br />
+        Fax: +49 (0)221 - 68 97 797<br />
         E-Mail: <a href="mailto:info@tanzimpulse-koeln.de" class="hover:text-ti_blue_accent ease-in-out transition-all duration-300">info@tanzimpulse-koeln.de</a><br />
         <br />
         <span class="uppercase tracking-widest text-xs">Bürozeiten</span><br />
