3feebd663f69a34140a90f4222d9ee7167f55101
Schaeferjo <mail@joschaefer.com>
2021-11-25 13:49:31 +0100

    fix: form error

--- src/lib/graphql/mutations.ts
+++ src/lib/graphql/mutations.ts
@@ -2,7 +2,7 @@ import { gql } from '$lib/helpers';
 import { seminarFragment } from './queries';
 export const UPSERT_TEILNEHMER = gql`
   ${seminarFragment}
-  mutation ($url: String!, $email: String!, $vorname: String!, $nachname: String!, $telefon: String, $adresse: String!, $datenverarbeitung: Boolean!, $anmerkung: String, $newsletter: Boolean!) {
+  mutation ($url: String!, $email: String!, $vorname: String!, $nachname: String!, $telefon: String!, $adresse: String!, $datenverarbeitung: Boolean!, $anmerkung: String, $newsletter: Boolean!) {
     teilnehmer: upsertTeilnehmer(
       where: {email: $email}
       upsert: {create: {vorname: $vorname, nachname: $nachname, email: $email, adresse: $adresse, telefon: $telefon, anmerkung: $anmerkung, datenverarbeitung: $datenverarbeitung, newsletter: $newsletter, seminare: {connect: {url: $url}}}, update: {vorname: $vorname, nachname: $nachname, email: $email, adresse: $adresse, telefon: $telefon, anmerkung: $anmerkung, datenverarbeitung: $datenverarbeitung, newsletter: $newsletter, seminare: {connect: {where: {url: $url}}}}}
