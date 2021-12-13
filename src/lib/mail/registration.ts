452bde638070ae9215a853b145e6a03ec55cbf4b
Schaeferjo <mail@joschaefer.com>
2021-12-13 17:17:17 +0100

    feat: update url and text

--- src/lib/mail/registration.ts
+++ src/lib/mail/registration.ts
@@ -15,7 +15,7 @@ const html = (teilnehmer: Teilnehmer, seminar: Seminar) => `
   <br>
   <hr>
   <br>
-  <p><strong>Tanzimpulse - Institut für Tanzimpulse</strong></p>
+  <p><strong>Tanzimpulse - Institut für Tanzpädagogik</strong></p>
   <p>
   Büroanschrift<br>
   Folwiese 20<br>
@@ -25,7 +25,7 @@ const html = (teilnehmer: Teilnehmer, seminar: Seminar) => `
   Tel:	0221 - 68 36 91<br>
   Fax: 	0221 - 689 77 97<br>
   E-Mail: <a href="mailto:info@tanzimpulse-koeln.de">info@tanzimpulse-koeln.de</a><br>
-  Web: 	<a href="https://www.tanzimpulse-koeln.de" target="_blank">tanzimpulse-koeln.de</a>
+  Web: 	<a href="https://www.tanzimpulse.de" target="_blank">tanzimpulse-koeln.de</a>
   </p>
   <p>
   Bürozeiten<br>
--- src/lib/mail/waitingList.ts
+++ src/lib/mail/waitingList.ts
@@ -13,7 +13,7 @@ Wir haben Sie auf die Warteliste gesetzt und werden Sie kontaktieren, wenn kurzf
 <br>
 <hr>
 <br>
-<p><strong>Tanzimpulse - Institut für Tanzimpulse</strong></p>
+<p><strong>Tanzimpulse - Institut für Tanzpädagogik</strong></p>
 <p>
 Büroanschrift<br>
 Folwiese 20<br>
@@ -23,7 +23,7 @@ Bettina Bierdümpel und Ute Luckey</p>
 Tel:	0221 - 68 36 91<br>
 Fax: 	0221 - 689 77 97<br>
 E-Mail: <a href="mailto:info@tanzimpulse-koeln.de">info@tanzimpulse-koeln.de</a><br>
-Web: 	<a href="https://www.tanzimpulse-koeln.de" target="_blank">tanzimpulse-koeln.de</a>
+Web: 	<a href="https://www.tanzimpulse.de" target="_blank">tanzimpulse-koeln.de</a>
 </p>
 <p>
 Bürozeiten<br>
