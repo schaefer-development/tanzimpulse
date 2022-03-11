5aba4b4ad7b6fdb4f4731047326e5960c2eb0970
Schaeferjo <mail@joschaefer.com>
2022-03-11 16:54:51 +0100

    feat: gottverdammter fuck!!!

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -43,8 +43,6 @@
     };
   };
 
-  import { fade } from 'svelte/transition';
-
   export const prerender = true;
 </script>
 
@@ -57,73 +55,94 @@
   <title>Start | Tanzimpulse</title>
 </svelte:head>
 
-<section>
-  <div class="container flex flex-col">
-    <Skeleton>
-      <Slideshow slot="mounted" />
-    </Skeleton>
-
-    <div class="cards my-10 grid grid-cols-1 lg:grid-cols-3 gap-10">
-      <div class="flex items-start">
-        <div class="bg-ti_blue_mat p-4 md:p-6 lg:p-8 text-white shadow-ti hover:shadow-ti_hover transition duration-300 ease-in-out">
-          <h2 class="font-bold text-2xl lg:text-3xl uppercase tracking-wide">Ausbildungen</h2>
-          <p class="py-4">Werde Tanzpädagogin/ Tanzpädagoge, geschult durch unser kompetentes Team und integriere den Tanz in den Beruf. Tanzvermittlung professionell und praxisnah. Beginn der Grundstufen zwei mal jährlich: im Frühjahr und im Herbst.</p>
-          <p class="py-4">
-            <Button href="{base}/ausbildungen" buttonstyle={'blue'}>Alle Ausbildungen</Button>
-          </p>
-          <p class="px-2 pb-2 uppercase tracking-wider font-bold text-sm text-ti_blue_accent text-center">Die nächsten drei Ausbildungen:</p>
-          <hr class="block border border-black w-full mb-2 opacity-20" />
-          <SeminarPreviews seminare={kommende.ausbildungen} color="blue" />
-        </div>
+<section class="relative w-full slide-content border border-black overflow-hidden">
+  <Skeleton>
+    <div id="LOADING" class="absolute z-10 top-0 w-full slide-content bg-warmGray-200" slot="loading">
+      <div class="flex justify-center items-center px-4 py-10 leading-6 transition ease-in-out duration-150">
+        <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
+          <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
+        </svg>
+        Loading ...
       </div>
-
-      <div class="flex items-start">
-        <div class="bg-ti_cyan_mat p-4 md:p-6 lg:p-8 text-white shadow-ti hover:shadow-ti_hover transition duration-300 ease-in-out">
-          <h2 class="font-bold text-2xl lg:text-3xl uppercase tracking-wide">Workshops</h2>
-          <p class="py-4">Immer aktuell sein, mit Impulsen für die tanzpädagogische Praxis für alle Tanzinteressierten die mit Kita-, Grundschulkindern, Jugendlichen und / oder Erwachsenen arbeiten.</p>
-          <p class="py-4">
-            <Button href="{base}/workshops" buttonstyle={'cyan'}>Alle Workshops</Button>
-          </p>
-          <p class="px-2 pb-2 uppercase tracking-wider font-bold text-sm text-ti_cyan_accent text-center">Die nächsten drei Workshops:</p>
-          <hr class="block border border-black w-full mb-2 opacity-20" />
-          <SeminarPreviews seminare={kommende.workshops} color="cyan" />
-        </div>
-      </div>
-
-      <div class="flex items-start">
-        <div class="bg-ti_green_mat flex flex-col p-8 text-white shadow-ti hover:shadow-ti_hover transition duration-300 ease-in-out">
-          <h2 class="font-bold text-2xl lg:text-3xl uppercase tracking-wide">Tanzraum Vermietung</h2>
-          <p class="py-4">Ein Raum mit angenehmer Atmosphäre für Weiterbildungen, Tanztrainings, Entspannung etc. mit Ausstattung für professionellen Unterricht.</p>
-          <p class="py-4">
-            <Button href="{base}/tanzraum" buttonstyle={'green'}>Mehr erfahren</Button>
-          </p>
-        </div>
-      </div>
+      <img class="object-cover opacity-0" src="/images/slideshow/ausbildungen.jpg" alt="Ausbildungen bei Tanzimpulse" />
     </div>
 
-    <div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
-      <!-- eyecatcher -->
-      {#each neuigkeiten as neuigkeit (neuigkeit.id)}
-        <DefaultCard>
-          <div class="graphcms_card_startpage">
-            <h2 class="ti_headline_blue_bold pb-4">{neuigkeit.ueberschrift}</h2>
-            {@html neuigkeit.inhalt.html}
-          </div>
-        </DefaultCard>
-      {/each}
+    <div id="MOUNTED" class="relative">
+      <Slideshow slot="mounted" />
+    </div>
+  </Skeleton>
+</section>
 
-      <DefaultCard>
-        <h2 class="ti_headline_blue_bold">Tanzimpulse stellt auf hybride Lehr- und Lernformate um</h2>
-        <p class="py-4">Hierfür erhalten wir eine Projekt-Förderung von:</p>
-        <div class="flex flex-col md:flex-row w-full justify-items-center ">
-          <div class="flex-1">
-            <img class="mx-auto" src="{base}/images/BKM_Web_de.gif" alt="Logo Die Bundesbeauftragte der Bundesregierung für Kultur und Medien" />
-          </div>
-          <div class="flex-1">
-            <img class="mx-auto" src="{base}/images/DVT.png" alt="Logo Dachverband Tanzen" />
-          </div>
-        </div>
-      </DefaultCard>
+<!-- -->
+
+<section id="3CARDS" class="cards my-10 grid grid-cols-1 lg:grid-cols-3 gap-10">
+  <div class="flex items-start">
+    <div class="bg-ti_blue_mat p-4 md:p-6 lg:p-8 text-white shadow-ti hover:shadow-ti_hover transition duration-300 ease-in-out">
+      <h2 class="font-bold text-2xl lg:text-3xl uppercase tracking-wide">Ausbildungen</h2>
+      <p class="py-4">Werde Tanzpädagogin/ Tanzpädagoge, geschult durch unser kompetentes Team und integriere den Tanz in den Beruf. Tanzvermittlung professionell und praxisnah. Beginn der Grundstufen zwei mal jährlich: im Frühjahr und im Herbst.</p>
+      <p class="py-4">
+        <Button href="{base}/ausbildungen" buttonstyle={'blue'}>Alle Ausbildungen</Button>
+      </p>
+      <p class="px-2 pb-2 uppercase tracking-wider font-bold text-sm text-ti_blue_accent text-center">Die nächsten drei Ausbildungen:</p>
+      <hr class="block border border-black w-full mb-2 opacity-20" />
+      <SeminarPreviews seminare={kommende.ausbildungen} color="blue" />
+    </div>
+  </div>
+
+  <div class="flex items-start">
+    <div class="bg-ti_cyan_mat p-4 md:p-6 lg:p-8 text-white shadow-ti hover:shadow-ti_hover transition duration-300 ease-in-out">
+      <h2 class="font-bold text-2xl lg:text-3xl uppercase tracking-wide">Workshops</h2>
+      <p class="py-4">Immer aktuell sein, mit Impulsen für die tanzpädagogische Praxis für alle Tanzinteressierten die mit Kita-, Grundschulkindern, Jugendlichen und / oder Erwachsenen arbeiten.</p>
+      <p class="py-4">
+        <Button href="{base}/workshops" buttonstyle={'cyan'}>Alle Workshops</Button>
+      </p>
+      <p class="px-2 pb-2 uppercase tracking-wider font-bold text-sm text-ti_cyan_accent text-center">Die nächsten drei Workshops:</p>
+      <hr class="block border border-black w-full mb-2 opacity-20" />
+      <SeminarPreviews seminare={kommende.workshops} color="cyan" />
+    </div>
+  </div>
+
+  <div class="flex items-start">
+    <div class="bg-ti_green_mat flex flex-col p-8 text-white shadow-ti hover:shadow-ti_hover transition duration-300 ease-in-out">
+      <h2 class="font-bold text-2xl lg:text-3xl uppercase tracking-wide">Tanzraum Vermietung</h2>
+      <p class="py-4">Ein Raum mit angenehmer Atmosphäre für Weiterbildungen, Tanztrainings, Entspannung etc. mit Ausstattung für professionellen Unterricht.</p>
+      <p class="py-4">
+        <Button href="{base}/tanzraum" buttonstyle={'green'}>Mehr erfahren</Button>
+      </p>
     </div>
   </div>
 </section>
+
+<!-- -->
+
+<section id="CARDS" class="grid grid-cols-1 lg:grid-cols-2 gap-10">
+  {#each neuigkeiten as neuigkeit (neuigkeit.id)}
+    <DefaultCard>
+      <div class="graphcms_card_startpage">
+        <h2 class="ti_headline_blue_bold pb-4">{neuigkeit.ueberschrift}</h2>
+        {@html neuigkeit.inhalt.html}
+      </div>
+    </DefaultCard>
+  {/each}
+  <DefaultCard>
+    <h2 class="ti_headline_blue_bold">Tanzimpulse stellt auf hybride Lehr- und Lernformate um</h2>
+    <p class="py-4">Hierfür erhalten wir eine Projekt-Förderung von:</p>
+    <div class="flex flex-col md:flex-row w-full justify-items-center ">
+      <div class="flex-1">
+        <img class="mx-auto" src="{base}/images/BKM_Web_de.gif" alt="Logo Die Bundesbeauftragte der Bundesregierung für Kultur und Medien" />
+      </div>
+      <div class="flex-1">
+        <img class="mx-auto" src="{base}/images/DVT.png" alt="Logo Dachverband Tanzen" />
+      </div>
+    </div>
+  </DefaultCard>
+</section>
+
+<style>
+  .slide-content {
+    width: 100%;
+    min-height: 300px;
+    max-height: 40vh;
+  }
+</style>
