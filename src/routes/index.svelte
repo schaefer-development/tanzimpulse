0bfa18c2eee5f5522da37f55003061b33d6b64ba
Robert Schäfer <git@roschaefer.de>
2022-03-13 18:20:46 +0100

    refactor: fun with flexbox

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -55,17 +55,16 @@
   <title>Start | Tanzimpulse</title>
 </svelte:head>
 
-<section class="relative w-full slide-content border border-black overflow-hidden">
+<section class="relative w-full slide-content overflow-hidden">
   <Skeleton>
-    <div id="LOADING" class="absolute z-10 top-0 w-full slide-content bg-warmGray-200" slot="loading">
-      <div class="flex justify-center items-center px-4 py-10 leading-6 transition ease-in-out duration-150">
+    <div id="LOADING" class="w-full slide-content bg-warmGray-200 flex justify-center items-center" slot="loading">
+      <div class="leading-6 transition ease-in-out duration-150 flex">
         <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
           <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
           <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
         </svg>
-        Loading ...
+        Wird geladen ...
       </div>
-      <img class="object-cover opacity-0" src="/images/slideshow/ausbildungen.jpg" alt="Ausbildungen bei Tanzimpulse" />
     </div>
 
     <Slideshow slot="mounted"/>
