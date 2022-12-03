92815b50106f73968bab575dab90df8be8dc1b0c
joschaefer <mail@joschaefer.com>
2022-12-03 15:34:18 +0100

    feat: increase contrast

--- src/lib/components/Button/Button.svelte
+++ src/lib/components/Button/Button.svelte
@@ -1,11 +1,13 @@
 <script>
-  export let buttonstyle = 'blue';
+  export let buttonstyle = 'default';
   export let href = null;
 
   const mapping = {
-    blue: 'border-ti_blue_accent text-ti_blue_accent hover:bg-ti_blue_mat-dark hover:text-ti_blue_accent-light focus:text-ti_blue_accent focus:bg-ti_blue_mat-dark focus:text-white',
-    cyan: 'border-ti_cyan_accent-light text-ti_cyan_accent hover:bg-ti_cyan_mat-dark hover:text-ti_cyan_accent focus:text-ti_cyan_accent focus:bg-ti_cyan_mat-dark focus:text-white',
-    green: 'border-ti_green_accent-light text-ti_green_accent hover:bg-ti_green_mat-dark hover:text-ti_green_accent hover:text-ti_green_accent focus:bg-ti_green_mat-dark focus:text-white ',
+    default: 'border-ti_blue_accent text-ti_blue_accent hover:bg-ti_blue_mat-dark hover:text-ti_blue_accent-light focus:text-ti_blue_accent focus:bg-ti_blue_mat-dark focus:text-white',
+
+    blue: 'border-white text-white hover:bg-ti_blue_accent hover:text-ti_blue_mat-dark',
+    cyan: 'border-white text-white hover:bg-ti_cyan_accent hover:text-ti_cyan_mat-dark',
+    green: 'border-white text-white hover:bg-ti_green_accent hover:text-ti_green_mat-dark',
     red: 'border-ti_red_accent-light text-ti_red_accent hover:bg-ti_red_mat-dark hover:text-ti_red_accent hover:text-ti_red_accent focus:bg-ti_red_mat-dark focus:text-white ',
     white: 'text-white border-white hover:bg-ti_red_mat-dark hover:text-white focus:bg-ti_red_mat-dark focus:text-white ',
     gray: 'text-gray-400 border-gray-400 cursor-not-allowed hover:text-gray-400 focus:text-gray-400'
