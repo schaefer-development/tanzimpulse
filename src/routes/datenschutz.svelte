0908bdd276283a1d9dc3a336483aa9359ee9bb11
joschaefer <mail@joschaefer.com>
2023-03-08 14:23:23 +0100

    feat: removed paragraph google analytics

--- src/routes/datenschutz.svelte
+++ src/routes/datenschutz.svelte
@@ -75,14 +75,6 @@
           <br />
           Ute Luckey, Geschäftsführung
         </p>
-        <hr class="block border border-coolGray-100 w-full mt-4 mb-8" />
-        <h2 class="ti_headline_blue_light mb-4">Google Analytics</h2>
-        <p>
-          Diese Website benutzt Google Analytics, einen Webanalysedienst der Google Inc. („Google“). Google Analytics verwendet sog. „Cookies“, Textdateien, die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie ermöglicht. Die durch den Cookie erzeugten Informationen über Ihre Benutzung dieser Website (einschließlich Ihrer IP-Adresse) wird an einen Server von Google in den USA übertragen und dort gespeichert. Google wird diese Informationen benutzen, um Ihre Nutzung der Website auszuwerten, um Reports über die Websiteaktivitäten für die Websitebetreiber zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen zu erbringen. Auch wird Google diese Informationen gegebenenfalls an Dritte übertragen, sofern dies gesetzlich vorgeschrieben oder soweit Dritte diese Daten im Auftrag von Google verarbeiten. Google wird in keinem Fall Ihre IP-Adresse mit anderen Daten, die von Google gespeichert
-          werden, in Verbindung bringen. Sie können die Installation der Cookies durch eine entsprechende Einstellung Ihrer Browser Software verhindern; wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht sämtliche Funktionen dieser Website voll umfänglich nutzen können. Durch die Nutzung dieser Website erklären Sie sich mit der Bearbeitung der über Sie erhobenen Daten durch Google in der zuvor beschriebenen Art und Weise und zu dem zuvor benannten Zweck einverstanden.
-          <br />
-          Die IP-Adresse, die an Google gesendet wird, wird zuvor anonymisiert, damit die gespeicherten Daten nicht mehr einer Person eindeutig zugeordnet werden können.
-        </p>
       </DefaultCard>
     </div>
   </div>
