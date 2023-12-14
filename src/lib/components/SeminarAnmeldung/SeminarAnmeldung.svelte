1b25c8b50120f79187b6f493c06dca5d7ac36b16
joschaefer <mail@joschaefer.com>
2023-12-14 12:13:39 +0100

    feat: removed 2-grid layout and replaced with flex based layout

--- src/lib/components/SeminarAnmeldung/SeminarAnmeldung.svelte
+++ src/lib/components/SeminarAnmeldung/SeminarAnmeldung.svelte
@@ -29,7 +29,9 @@
 </script>
 
 <section>
-  <div class="container mx-auto grid grid-cols-2 lg:grid-cols-2 gap-10">
+  <!--   <div class="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10">
+-->
+  <div class="container mx-auto mx-auto w-full max-w-3xl gap-10">
     <div class="__long_description">
       <SeminarCard {seminar}>
         <div slot="beschreibung">
@@ -62,16 +64,16 @@
 
           <p class="h-8 block" />
           <VeranstaltungsOrt showMap={true} veranstaltungsort={seminar.veranstaltungsort} />
-          <div class="pt-10 pb-6">
+          <div class="pt-10 pb-4">
             <h2 class="ti_headline_blue_bold">Anmeldung</h2>
             <div class="flex py-6">
-              <div class="icon w-2/12 flex justify-center items-center text-blue-600">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+              <div class="icon flex justify-center items-center text-ti_red_accent">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 m-4">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
                 </svg>
               </div>
               <div class="w-9/12">
-                <p>Die Anmeldung erfolgt auf der Webseite unseres Kooperationspartners, der VHS.</p>
+                <p class="py-2"><span class="font-bold text-ti_red_accent">INFO:</span> Die Anmeldung erfolgt auf der Webseite unseres Kooperationspartners, der VHS Bergisch Gladbach.</p>
               </div>
             </div>
             <a href={seminar.linktovhs} target="_blank" aria-label="Link zur VHS" class="block text-center border-ti_blue_accent text-ti_blue_accent hover:bg-ti_blue_accent hover:text-white p-4 w-full rounded-full border-2 font-extrabold text-sm tracking-widest uppercase outline-none shadow-sm hover:shadow-ti focus:outline-none focus:ring-0 transition duration-300 ease-in-out">Jetzt diesen Kurs über die VHS buchen</a>
