82da9e9e1e235d64d39e6ac614c3bfeb82208619
joschaefer <mail@joschaefer.com>
2025-03-05 21:07:36 +0100

    feat: add some pages

---
+++ src/routes/ausbildungen/+page.svelte
@@ -0,0 +1,66 @@
+<script context="module" lang="ts">
+	import DefaultCard from '$lib/components/DefaultCard/DefaultCard.svelte';
+	const now = new Date();
+	export const prerender = true;
+  </script>
+
+
+<svelte:head>
+  <title>Ausbildungen</title>
+</svelte:head>
+
+<section>
+	<div class="container">
+	  <div class="flex w-full lg:w-1/2">
+		<DefaultCard>
+		  <h2 class="ti_headline_blue_bold">Ausbildungen</h2>
+		  <h3 class="ti_headline_blue_light mb-4">Zusatzqualifikation zur Tanzpädagogin / zum Tanzpädagogen</h3>
+		  <p class="py-4">Unsere dreistufige Ausbildung zur Tanzpädagogin/zum Tanzpädagogen ist seit 1994 etabliert. Als die Experten für Pädagogik im Tanz vermittelt das Team der TANZIMPULSE, wie der Unterricht soziale Aspekte berücksichtigt, wie die individuellen Gestaltungs-und Bewegungsmöglichkeiten erlebt und erweitert werden können.</p>
+		  <p class="py-4">Wir verstehen Tanz als eine Kunstform, die ein Baustein der kulturellen Bildung darstellt. Zur erfolgreichen Umsetzung der Ausbildungsinhalte gehören Reflexion und Coaching durch das Ausbildungsteam.</p>
+  
+		  <p class="py-2">
+			<Accordion id="1">
+			  <span slot="headline">1. Die Grundstufen</span>
+			  <div slot="content">
+				<h3 class="font-bold py-4">a) Grundstufe/Alters-und Zielgruppen übergreifend</h3>
+				<p class="pb-8">in der einjährigen Grundstufenausbildung ist das Ziel die Vermittlung von Grundlagen in Tanzstilen wie Hip Hop, Moderner Tanz, Tanzimprovisation, Jazztanz und Musical; die Vermittlung von tanzpädagogischer Methoden-Vielfalt mit Praxisbezug; Tanz als Medium in der Arbeit mit Kindern, Jugendlichen, Erwachsenen/Senioren und in Inklusionsgruppen.</p>
+				<h3 class="font-bold py-4">b) Grundstufe/Schwerpunkt Kindertanz</h3>
+				<p class="pb-4">für die Arbeit mit Kindern von U3 bis 11 Jahren</p>
+				<p class="pb-4">In dieser einjährigen Qualifikation steht die ästhetisch-kulturelle Bildung im Mittelpunkt. Visuell, auditiv und kinästhetisch werden die Verknüpfungen von Musik, Sprache und Tanz zur Unterstützung der kindlichen Entwicklung eingesetzt. Ausgangspunkt ist eine Tanzpädagogik, die sich an der altersgemäßen Entwicklung der Kinder orientiert. Tanz verbindet das Erkennen und Erfahren mit Erleben und Empfinden!</p>
+				<p class="pb-4">Nach erfolgreichem Abschluss einer Grundstufe wird ein Zertifikat erteilt, in dem Umfang und Inhalte der Zusatzqualifikation detailliert aufgeführt sind. Dies berechtigt zur Teilnahme an der Aufbaustufe.</p>
+				<p class="pb-4">Die Grundstufen a) + b) können unabhängig von den anderen Bausteinen als abgeschlossene Zusatzausbildung besucht werden.</p>
+				<p class="pb-4">Ausbildungsinformationen und Übungsvideos werden zur Verfügung gestellt.</p>
+				<p class="py-8">
+				  <a href="#ausbildungen" aria-label="Mehr" class="border-ti_blue_accent text-ti_blue_accent hover:bg-ti_blue_accent hover:text-white p-4 w-full rounded-full border-2 font-extrabold text-sm tracking-widest uppercase outline-none shadow-sm hover:shadow-ti focus:outline-none focus:ring-0 transition duration-300 ease-in-out">Jetzt Kurse entdecken</a>
+				</p>
+			  </div>
+			</Accordion>
+			<Accordion id="2">
+			  <span slot="headline">2. Die Aufbaustufen</span>
+			  <div slot="content">
+				<p>...dienen den Tanzpädagogen/innen sowie Quereinsteiger/innen dazu, ihre persönlichen tänzerischen Schwerpunkte (aus dem 1. Jahr) individuell zu bearbeiten und zu trainieren. Tanztheorie, Tanzwissenschaft, Anatomie + Körperarbeit werden vertieft; in einer Projektphase erprobt sich jede/r Teilnehmer/in als Tänzer/in, Choreograf/in oder Projektleitung.</p>
+				<p class="py-2">Nach erfolgreichem Abschluss der Aufbaustufe wird ein Zertifikat erteilt, in dem Umfang und Inhalte der Tanzpädagogik-Ausbildung detailliert aufgeführt sind</p>
+				<p class="py-8">
+				  <a href="#ausbildungen" aria-label="Mehr" class="border-ti_blue_accent text-ti_blue_accent hover:bg-ti_blue_accent hover:text-white p-4 w-full rounded-full border-2 font-extrabold text-sm tracking-widest uppercase outline-none shadow-sm hover:shadow-ti focus:outline-none focus:ring-0 transition duration-300 ease-in-out">Jetzt Kurse entdecken</a>
+				</p>
+			  </div>
+			</Accordion>
+			<Accordion id="3">
+			  <span slot="headline">3. Die Aufbaustufe mit Diplom (TI)</span>
+			  <div slot="content">
+				<p>...beinhaltet die Auseinandersetzung mit einem selbstgewählten tanzwissenschaftlichen Thema, mit der Entwicklung eines eigenen tanzpädagogischen Profils sowie der Planung, Durchführung und Reflexion eines Workshops. Sie schließt mit einer Diplomarbeit und einem Colloquium ab.</p>
+				<p class="py-2">Nach erfolgreichem Abschluss der Aufbaustufe mit Diplom (TI) wird eine Diplomurkunde (TI) erteilt.</p>
+				<p class="py-8">
+				  <a href="#ausbildungen" aria-label="Mehr" class="border-ti_blue_accent text-ti_blue_accent hover:bg-ti_blue_accent hover:text-white p-4 w-full rounded-full border-2 font-extrabold text-sm tracking-widest uppercase outline-none shadow-sm hover:shadow-ti focus:outline-none focus:ring-0 transition duration-300 ease-in-out">Jetzt Kurse entdecken</a>
+				</p>
+			  </div>
+			</Accordion>
+		  </p>
+		</DefaultCard>
+	  </div>
+	  <Cite />
+	</div>
+  </section>
+  <span id="ausbildungen" />
+  <SeminarDashboard {seminare} />
+  
---
+++ src/routes/kontakt/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+	export const prerender = true;
+  </script>
+  
+  <svelte:head>
+	<title>Kontakt</title>
+  </svelte:head>
+  
+  <section>
+	<div class="container">
+	  <div class="grid grid-cols-1 gap-10">
+		<div class="grid grid-cols-1 lg:grid-cols-2 gap-10">
+		  <div class="flex items-start">
+			<div class="w-full p-4 sm:p-6 md:p-8 lg:p-10 bg-ti_green_mat text-white shadow-ti">
+			  <h2 class="uppercase text-2xl lg:text-3xl tracking-wider font-bold text-white mb-4">Anschrift</h2>
+			  <h2 class="uppercase text-2xl lg:text-3xl tracking-wider font-extralight text-white">Büro</h2>
+			  <p class="py-2">
+				Folwiese 20<br />
+				51069 Köln
+			  </p>
+  
+			  <p class="pt-2 pb-4">
+				Telefon: +49 (0)221 - 68 36 91
+				<br />
+				Fax: +49 (0)221 - 68 97 797
+				<br />
+				E-Mail: <a class="font-bold pt-2 uppercase text-xs tracking-widest hover:text-black ease-in-out transition-all duration-300" href="mailto:info@tanzimpulse-koeln.de">info@tanzimpulse-koeln.de</a>
+			  </p>
+			  <p class="mb-8">
+				<span class="uppercase font-extrabold tracking-widest text-xs">Bürozeiten</span><br />
+				Montag bis Freitag: 9:00 - 13:00 Uhr<br />
+				Dienstag: 18:00 - 20:00 Uhr
+			  </p>
+			</div>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </section>
+  
