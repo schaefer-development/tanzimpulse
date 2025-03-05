fa0427c0e7e4794d23dd82cb156eb68da32d8d50
joschaefer <mail@joschaefer.com>
2025-03-05 20:42:13 +0100

    feat: install font, tailwind settings

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import Logo from '$lib/components/Logo/Logo.svelte';
-	/* import '@fontsource/poppins/200.css';
-  import '@fontsource/poppins/400.css';
-  import '@fontsource/poppins/700.css';
-  import '@fontsource/poppins/800.css';*/
+	import '@fontsource/poppins/200.css';
+	import '@fontsource/poppins/400.css';
+	import '@fontsource/poppins/700.css';
+	import '@fontsource/poppins/800.css';
 	import Header from '$lib/components/Header/Header.svelte';
 	import Footer from '$lib/components/Footer/Footer.svelte';
 	import '../app.css';
@@ -12,52 +12,45 @@
 	let { children } = $props();
 </script>
 
-<div class="app flex min-h-screen flex-col justify-between">
+<div class="flex min-h-screen flex-col justify-between">
 	<Header />
-
 	<main class="bg-warmGray-200 relative top-0 mb-auto h-auto">
 		<div id="BUBBLES" class="absolute z-0 h-full w-full overflow-hidden opacity-30">
 			<img src="{base}/images/bg.svg" alt="background" />
 		</div>
-		{@render children()}
+		<div id="LOGO" class="bg-white pt-14 pb-48">
+			<div
+				class="relative z-10 container mx-auto flex flex-col items-center px-0 md:flex-row md:px-6"
+			>
+				<div class="mb-6 ml-4 w-48 md:mb-0 md:w-64">
+					<a
+						aria-label="Logo Tanzimpulse - Institut für Tanzpädagogik"
+						sveltekit:prefetch
+						class="text-ti_blue_mat hover:text-ti_blue_accent focus:text-ti_blue_accent transition duration-300 ease-in-out outline-none focus:ring-0 focus:outline-none"
+						href="{base}/"><Logo /></a
+					>
+				</div>
+				<div class="px-8">
+					<a
+						aria-label="Logo Tanzimpulse - Institut für Tanzpädagogik"
+						sveltekit:prefetch
+						href="{base}/"
+						class="text-ti_blue_mat hover:text-ti_blue_accent focus:text-ti_blue_accent text-xl font-extralight tracking-wider uppercase transition duration-300 ease-in-out outline-none focus:ring-0 focus:outline-none sm:text-2xl md:text-3xl lg:text-4xl"
+					>
+						<span class="font-bold">Tanzimpulse</span> – Institut für Tanzpädagogik
+						<span
+							class="text-ti_blue_accent block text-base font-normal tracking-wide normal-case sm:text-lg md:text-xl lg:text-2xl"
+							>Staatlich anerkanntes Weiterbildungsinstitut</span
+						>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div id="CONTENT">
+			<div class="relative container mx-auto -mt-32 mb-20 px-2 lg:px-6">
+				{@render children()}
+			</div>
+		</div>
 	</main>
-
 	<Footer />
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,81 @@
 <script lang="ts">
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcomeFallback from '$lib/images/svelte-welcome.png';
+	import DefaultCard from '$lib/components/DefaultCard/DefaultCard.svelte';
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Start | Tanzimpulse</title>
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
+<section class="relative grid grid-cols-1 gap-12 pb-12 lg:grid-cols-2">
+	<div class="cardWrap grow">
+		<div class="bg-ti_blue_mat hover:shadow-ti transition duration-300 ease-in-out">
 			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcomeFallback} alt="Welcome" />
+				<source
+					srcset={motifAusbildungen.replace('.jpg', '.webp')}
+					type="webp"
+					class="h-72 w-full object-cover object-center"
+				/>
+				<img
+					src={motifAusbildungen}
+					class="h-72 w-full object-cover object-top"
+					alt="Werde Tanzpädagogin/ Tanzpädagoge bei Tanzimpulse "
+				/>
 			</picture>
-		</span>
+			<h2 class="px-8 pt-8 pb-4 text-2xl font-bold tracking-wide text-white uppercase lg:text-3xl">
+				Ausbildungen
+			</h2>
+			<p class="px-8 text-white">
+				Werde Tanzpädagogin/ Tanzpädagoge, geschult durch unser kompetentes Team und integriere den
+				Tanz in den Beruf. Tanzvermittlung professionell und praxisnah. Beginn der Grundstufen zwei
+				mal jährlich: im Frühjahr und im Herbst.
+			</p>
+			<p class="p-8">
+				<a
+					href="{base}/ausbildungen"
+					aria-label="Mehr"
+					class="hover:bg-ti_blue_accent hover:text-ti_blue_mat-dark hover:shadow-ti block w-full rounded-full border-2 border-white p-4 text-center text-sm font-extrabold tracking-widest text-white uppercase shadow-sm transition duration-300 ease-in-out outline-none focus:ring-0 focus:outline-none"
+					>Alle Ausbildungen</a
+				>
+			</p>
+		</div>
+	</div>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<div class="cardWrap grow">
+		<div class="bg-ti_cyan_mat hover:shadow-ti transition duration-300 ease-in-out">
+			<picture>
+				<source srcset={motifWorkshops.replace('.jpg', '.webp')} type="webp" />
+				<img
+					src={motifWorkshops}
+					class="h-72 w-full object-cover object-center"
+					alt="Werde Tanzpädagogin/ Tanzpädagoge bei Tanzimpulse "
+				/>
+			</picture>
+			<h2 class="px-8 pt-8 pb-4 text-2xl font-bold tracking-wide text-white uppercase lg:text-3xl">
+				Workshops
+			</h2>
+			<p class="px-8 text-white">
+				Immer aktuell sein, mit Impulsen für die tanzpädagogische Praxis für alle Tanzinteressierten
+				die mit Kita-, Grundschulkindern, Jugendlichen und/ oder Erwachsenen arbeiten.
+			</p>
+			<p class="p-8">
+				<a
+					href="{base}/workshops"
+					aria-label="Mehr"
+					class="hover:bg-ti_cyan_accent hover:text-ti_cyan_mat-dark hover:shadow-ti block w-full rounded-full border-2 border-white p-4 text-center text-sm font-extrabold tracking-widest text-white uppercase shadow-sm transition duration-300 ease-in-out outline-none focus:ring-0 focus:outline-none"
+					>Alle Workshops</a
+				>
+			</p>
+		</div>
+	</div>
 </section>
 
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<section id="CARDS" class="grid grid-cols-1 gap-10 lg:grid-cols-2">
+	{#each neuigkeiten as neuigkeit (neuigkeit.id)}
+		<DefaultCard>
+			<div class="graphcms_card_startpage">
+				<h2 class="ti_headline_blue_bold pb-4">{neuigkeit.ueberschrift}</h2>
+				{@html neuigkeit.inhalt.html}
+			</div>
+		</DefaultCard>
+	{/each}
+</section>
