36a11777319c25752d6cff5da936d8fcb386a923
Robert Schäfer <git@roschaefer.de>
2025-03-10 01:31:56 +0800

    fix remaining typescript issue

--- src/routes/[seminarFormat]/[url]/+page.server.ts
+++ src/routes/[seminarFormat]/[url]/+page.server.ts
@@ -5,6 +5,7 @@ import { api } from '$lib/graphql/api';
 import { SEMINAR } from '$lib/graphql/queries';
 import { dev } from '$app/environment';
 import { overbooked } from '$lib/helpers';
+import * as SMTPTransport from "nodemailer/lib/smtp-transport";
 import nodemailer from 'nodemailer';
 import { waitingListMessage, registrationMessage } from '$lib/mail';
 import sanitizeHtml from 'sanitize-html';
@@ -25,9 +26,9 @@ export const load: PageServerLoad = async ({ params }) => {
 	return { seminar };
 };
 
-const transporter = nodemailer.createTransport({
+const transportOptions: SMTPTransport.Options = {
 	host: SMTP_HOST,
-	port: SMTP_PORT,
+	port: parseInt(SMTP_PORT),
 	auth: {
 		user: SMTP_USERNAME,
 		pass: SMTP_PASSWORD
@@ -35,7 +36,8 @@ const transporter = nodemailer.createTransport({
 	tls: {
 		rejectUnauthorized: !dev
 	}
-});
+}
+const transporter = nodemailer.createTransport(transportOptions);
 
 const sendConfirmation = async (teilnehmer: Teilnehmer) => {
 	const {
