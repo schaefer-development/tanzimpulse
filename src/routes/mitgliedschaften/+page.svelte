694163ef2f73e20d0d045398bfa4b181d71e90dd
joschaefer <mail@joschaefer.com>
2025-12-03 12:57:24 +0100

    feat: new conformation of membership 2025

--- src/routes/mitgliedschaften/+page.svelte
+++ src/routes/mitgliedschaften/+page.svelte
@@ -108,9 +108,9 @@
 					> kann somit beantragt werden.
 				</p>
 				<p class="py-2">
-					<a class="hyperlink" href="{base}/content/2024_Mitgliedsbescheinigung.pdf" target="_blank"
+					<a class="hyperlink" href="{base}/content/2025_Mitgliedsbescheinigung.pdf" target="_blank"
 						>Mitgliedsbescheinigung der Qualitätsgemeinschaft Berufliche Bildung Region Köln e.V.
-						(PDF, 167KB)</a
+						(PDF, 383KB)</a
 					>
 				</p>
 			</DefaultCard>
