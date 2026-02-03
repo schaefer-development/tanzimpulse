84c38b65f3b96dc632fb8f2e6e8fbf2a3e66bf6e
joschaefer <mail@joschaefer.com>
2026-02-03 11:57:52 +0100

    feat: update content page impressum

--- src/routes/impressum/+page.svelte
+++ src/routes/impressum/+page.svelte
@@ -13,53 +13,78 @@
 			<DefaultCard>
 				<h2 class="ti_headline_blue_bold">Impressum</h2>
 
-				<h2 class="ti_headline_blue_light">Tanzimpulse bei Bildungswerk interKultur Bonn</h2>
-				<p class="py-2">Herausgeber: Aneta Göbel</p>
+				<h2 class="ti_headline_blue_light">Bildungswerk interKultur</h2>
+				<p class="pt-2 pb-6">staatlich anerkannte Einrichtung der Weiterbildung</p>
 				<p class="py-2">
-					Friesdorfer Straße 151<br />
+					Friesdorfer Str. 151<br />
+					53175 Bonn <br />
+					Tel. 0228 - 433 14 61<br />
+					Fax: 0228 - 433 14 62<br />
 
-					53175 Bonn
-				</p>
-				<p class="py-2">
-					Telefon: 0228 - 433 14 61
-					<br />
-					Fax: 0228 - 433 14 62
-					<br />
 					E-Mail:
 					<a class="hyperlink" href="mailto:tanzimpulse@ik-bonn.de">tanzimpulse@ik-bonn.de</a>
+					<br />
+
+					www.ik-bonn.de<br />
+				</p>
+
+				<p class="py-2">
+					Bankverbindung: Sparkasse KölnBonn<br />
+					IBAN DE71 3705 0198 1902 2833 06<br />
+					BIC COLSDE33XXX<br />
 				</p>
 				<p class="py-2">
-					Fotonachweise: Hauke Wintjen, Sascha Etzbach, Bettina Bierdümpel und Maria Luckey
+					Träger: Verein für Weiterbildung und Friedensarbeit e.V., Bonn<br />
+					Vereins-Register-Nr. VR 8569<br /> Amtsgericht Bonn<br />
+					1.Vorsitzende: Jana Weiler<br />
+					Steuernr.: 335/5797/0592<br />
 				</p>
 				<p class="py-2">
-					TANZIMPULSE sind nach § 9 AWBG als staatliche Weiterbildungs-Einrichtung anerkannt.
+					Fotonachweise : Fotonachweise: Hauke Wintjen, Sascha Etzbach, Bettina Bierdümpel und Maria
+					Luckey
+				</p>
+				<p class="py-2">
+					Tanzimpulse sind nach § 9 AWBG als staatliche Weiterbildungs-Einrichtung anerkannt.
 					Sonder- oder Bildungsurlaub kann somit beantragt werden. Die gesamte Ausbildungsgebühr
-					wird in der Regel von den Finanzämtern als Steuer mindernde Ausgaben anerkannt.
-				</p>
-				<p class="py-2">
-					Zu den <a
+					wird in der Regel von den Finanzämtern als Steuer mindernde Ausgaben anerkannt.<br />
+
+					Zu den
+					<a
 						class="hyperlink"
 						href="https://www.bildungsurlaub.de/bildungsurlaub-angebote/grundstufe-tanzpaedagogik/148025"
 						rel="noreferrer"
 						target="_blank">Fördermöglichkeiten</a
-					>.
+					>
 				</p>
-				<p class="py-2">© Bildungswerk interKultur Bonn 2002 - {now.getFullYear()}</p>
-				<hr class="mt-4 mb-8 block w-full border border-neutral-200" />
-				<h2 class="ti_headline_blue_light mb-4">Haftungsausschluss</h2>
+				<p class="pt-4 pb-2">
+					<strong>Haftung entsprechend Digitale-Dienste-Gesetz (DDG) Datenschutz</strong><br />Alle
+					dem Bildungswerk interKultur im Zuge einer Anmeldung oder anderer Eingabemöglichkeiten
+					mitgeteilten Daten werden ausschließlich für die jeweils angegebenen Zwecke verwendet und
+					nicht an Dritte weitergegeben.
+				</p>
+				<p class="pt-4 pb-2">
+					<strong>Urheberrechte Inhalte, Copyright</strong>
+					<br />Die über die Internetpräsenz des Bildungswerk interKultur www.ik-bonn.de angebotenen
+					Inhalte sind urheberrechtlich geschützt. Jede Nutzung unterliegt den geltenden
+					Urheberrechts- und anderen Schutzgesetzen. Der Nutzer verpflichtet sich, eventuelle
+					Schutzrechte nicht zu verletzen. Sofern im Einzelfall nichts anderes vereinbart ist, hat
+					der Nutzer lediglich das Recht zur Anfertigung einer angemessenen Zahl von Kopien von
+					Teilen der Inhalte zum persönlichen Gebrauch. Dabei ist sicherzustellen, dass alle
+					Hinweise in den Inhalten auf die Schutzrechte bestehen bleiben. Es ist insbesondere
+					untersagt, die über die Internetpräsenz www.ik-bonn.de angebotenen Inhalte für
+					kommerzielle Zwecke zu verwenden. Missbrauch wird verfolgt.
+				</p>
 				<p class="py-2">
-					Diese Webseite wurde mit größtmöglicher Sorgfalt zusammengestellt. Wir bemühen uns alle
-					Angaben stets auf dem aktuellen Stand zu halten. Eine Haftung oder Garantie für die
-					Richtigkeit und Vollständigkeit der zur Verfügung gestellten Informationen und Daten kann
-					aber nicht gegeben werden. Bildungswerk interKultur Bonn schließt jegliche Haftung für
-					Schäden, die direkt oder indirekt aus der Benutzung dieser Website entstehen, aus.
+					Die EU-Kommission stellt ab 15. Februar 2016 eine Plattform zur Online-Streitbeilegung
+					bereit, die Kunden und Händlern helfen soll, Streitigkeiten außergerichtlich zu klären.
+					Die Plattform ist über folgenden Link zu erreichen: <a
+						class="hyperlink"
+						href="http://ec.europa.eu//consumers/odr/"
+						target="_blank">http://ec.europa.eu//consumers/odr/</a
+					>
 				</p>
-				<p class="py-2">
-					Gemäß dem Urteil vom 12. Mai 1998 "Haftung für Links" (312 O 85/98) des Landgerichts
-					Hamburg distanziert sich Bildungswerk interKultur Bonn ausdrücklich von allen Inhalten
-					sämtlicher gelinkter Seiten. Für den Inhalt der verlinkten Seiten sind ausschließlich
-					deren Betreiber verantwortlich.
-				</p>
+				<p class="py-2">Letzte Aktualisierung: 12.08.2024</p>
+
 				<hr class="mt-4 mb-8 block w-full border border-neutral-200" />
 				<h3 class="font-bold">Webdesign und Entwicklung</h3>
 				<p class="py-2">
